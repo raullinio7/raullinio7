118813e6ae29178a5af000d73068caa1df8fdb91
Raul Linio Alonso <rlinioal@emeal.nttdata.com>
2024-02-16 17:58:32 +0100

    Initial Commit

---
+++ RandomUsersApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RandomUsersApp
+//
+//  Created by Raul Linio Alonso on 16/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomUsersApp/RandomUsersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersAppApp.swift
+//  RandomUsersApp
+//
+//  Created by Raul Linio Alonso on 16/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
